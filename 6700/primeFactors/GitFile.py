f41d0e15445ff7929aea32c3560077dce350ea1d
sah0017 <shammond@faulkner.edu>
2014-07-24 12:26:04 -0500

    eleventh green light

--- 6700/primeFactors/GitFile.py
+++ 6700/primeFactors/GitFile.py
@@ -21,24 +21,30 @@ class GitFile(object):
         Constructor
         '''
         self.myCommits = []
+        self.myTransformations = []
 
 
     def readGitFile(self, fileName):
         ## subprocess.call("git log -p -m > logfile")
-        myList = []
+        
         gitFile = codecs.open(fileName, encoding='utf-16')
         for line in gitFile:
             if line[0] == '+':
-                myList.append('New line')
+                self.myTransformations.append('New line')
             if line[0] == '-':
-                myList.append('line removed')
+                self.myTransformations.append('line removed')
             if line.find("Initial") > -1:
-                myList.append(myTrans.NEWFILE)
+                self.myTransformations.append(myTrans.NEWFILE)
+                self.newCommit = Commit.Commit()
+                self.myCommits.append(self.newCommit)
             if line.find("pass") > -1:
-                myList.append(myTrans.NULL)
+                self.myTransformations.append(myTrans.NULL)
         gitFile.close()
-        return myList
+
     
     def getCommits(self):
         return self.myCommits
+    
+    def getTransformations(self):
+        return self.myTransformations
     
--- 6700/primeFactors/testGitAnalysis.py
+++ 6700/primeFactors/testGitAnalysis.py
@@ -15,12 +15,14 @@ class Test(unittest.TestCase):
         self.myGitFile = GitFile.GitFile()
 
     def testLargeGitFile(self):
-        self.assertEqual(self.myGitFile.readGitFile("c:\\Users\\susanha\\git\\6700test\\revLogFile-short"),['New line', 'line removed', self.myTrans.NULL,'New line'])
-
+        myTransformations = []
+        self.myGitFile.readGitFile("c:\\Users\\susanha\\git\\6700test\\revLogFile-short")
+        self.assertEqual(self.myGitFile.getTransformations(),['New line', 'line removed', self.myTrans.NULL,'New line'])
+        
     def testGitFileCommits(self):
         myCommits = []
         self.myGitFile.readGitFile("c:\\Users\\susanha\\git\\6700test\\revLogfile")
-        self.assertEqual(len(self.myGitFile.getCommits()), 0)
+        self.assertEqual(len(self.myGitFile.getCommits()), 1)
 
 if __name__ == "__main__":
     #import sys;sys.argv = ['', 'Test.testGitFile1']
