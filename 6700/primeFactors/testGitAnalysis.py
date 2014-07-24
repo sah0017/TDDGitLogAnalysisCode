7306f07e926b1a5578aa9959862b8de3214c2901
sah0017 <shammond@faulkner.edu>
2014-07-24 13:27:40 -0500

    twelfth green light

--- 6700/primeFactors/testGitAnalysis.py
+++ 6700/primeFactors/testGitAnalysis.py
@@ -22,7 +22,25 @@ class Test(unittest.TestCase):
     def testGitFileCommits(self):
         myCommits = []
         self.myGitFile.readGitFile("c:\\Users\\susanha\\git\\6700test\\revLogfile")
-        self.assertEqual(len(self.myGitFile.getCommits()), 1)
+        self.assertEqual(len(self.myGitFile.getCommits()), 7)
+    ''' 
+    def testGitFileCommitsLOC(self):
+        myCommits = []
+        self.myGitFile.readGitFile("c:\\Users\\susanha\\git\\6700test\\revLogfile")
+        myCommits = self.myGitFile.getCommits()
+        self.assertEqual((myCommits[1]), 7)
+    '''         
+    def testGitFiles(self):
+        myFiles = []
+        self.myGitFile.readGitFile("c:\\Users\\susanha\\git\\6700test\\revLogfile")
+        self.assertEqual(len(self.myGitFile.getFiles()),4)
+
+    def testGitFileNames(self):
+        myFiles = []
+        self.myGitFile.readGitFile("c:\\Users\\susanha\\git\\6700test\\revLogfile")
+        myFiles = self.myGitFile.getFiles()
+        self.assertEqual(myFiles[0].getFileName(),'testPrimeFactor.py')
+
 
 if __name__ == "__main__":
     #import sys;sys.argv = ['', 'Test.testGitFile1']
