ec39698bcdf9c74761d70f7240f55e34d7ec7017
sah0017 <shammond@faulkner.edu>
2014-08-11 09:59:58 -0500

    twentyfifth green light

---
+++ 6700/primeFactors/testFuncs.py
@@ -0,0 +1,11 @@
+'''
+Created on Jun 26, 2014
+
+@author: susanha
+'''
+
+if __name__ == '__main__':
+    fp = open("C:\EclipsePhD\\HW1\\abd0001\\abd0001\src\HW1\statistics.py")
+    code = fp.read()
+    print code
+    
